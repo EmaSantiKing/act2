24f0b8ec6acd068c85b004aafe97a965174d8aa1
King <rey24vegas@gmail.com>
2025-05-07 20:17:01 -0300

    Add files via upload

---
+++ modulos.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function) {
+document.getElementById("header").innerHTML =
+<header>
+    <img src="logo" alt="logo" width="80"></img>
+    <button id ="menu">-</button>
+    <nav id="nav">
+        <ul class="nav-list">
+            <li><a href="juegos">juegos</a></li>
+            <li><a href="consola">consola</a></li>
+            <li><a href="perifericos">perifericos</a></li>
+        </ul>
+    </nav>
+</header>
+}
